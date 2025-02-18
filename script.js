86d07f5320f7d5689a176d6fb87e7b73da039ad9
nikita <dashgerot@gmail.com>
2025-02-19 03:19:57 +0700

    1

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const darkButton = document.querySelector('#darkButton');
 
 darkButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode')
-})
+
+    if (gradientText) {
+        gradientText.style.background = ''; // Убираем градиент
+        gradientText.style.color = ''; // Возвращаем стандартный цвет
+    }
+});
 
 const gradientText = document.querySelector('h1');
 
-gradientText.addEventListener('mousemove', (e) => {
+gradientText.addEventListener('pointermove', (e) => {
     const { offsetX, offsetY } = e;
     const { offsetWidth, offsetHeight } = gradientText;
 
@@ -20,5 +25,15 @@ gradientText.addEventListener('mousemove', (e) => {
       white 100%)`;
     gradientText.style.webkitBackgroundClip = "text";
     gradientText.style.color = "transparent";
+
+     if (document.body.classList.contains('dark-mode')) {
+         gradientText.style.background = `radial-gradient(circle at ${xPercent}% ${yPercent}%, 
+      yellow 0px, 
+      yellow 40px, 
+      black 20px, 
+      black 100%)`;
+         gradientText.style.webkitBackgroundClip = "text";
+         gradientText.style.color = "transparent";
+     }
 });
 
